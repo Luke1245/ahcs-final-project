a91078c6dece72f3ac33dd0736d0a7567793e419
Luke1245 <124802535+Luke1245@users.noreply.github.com>
2023-09-11 08:34:33 +0000

    Replit is a mediocre online IDE

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+  return "<p>Hello, World!</p>"
+
+
+app.run(host='0.0.0.0', port=8080)
